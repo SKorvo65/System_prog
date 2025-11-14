c0cc1e71545680e74b6937efbfc4760b938ef5ff
SKorvo65 <zab.vlad2014@yandex.ru>
2025-11-14 09:39:51 +0900

    Add files via upload

---
+++ CalcRunner/CalcRunner/CalcRunner.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CalcRunner
+{
+    partial class CalcRunner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.myProcess = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(123, 129);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(226, 116);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(391, 129);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(226, 116);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
+            this.myProcess.Exited += new System.EventHandler(this.process1_Exited);
+            // 
+            // CalcRunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "CalcRunner";
+            this.Text = "Calculator Runner";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Diagnostics.Process myProcess;
+    }
+}
+
---
+++ CalcRunner/CalcRunner/CalcRunner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CalcRunner
+{
+    public partial class CalcRunner : Form
+    {
+        public CalcRunner()
+        {
+            InitializeComponent();
+
+            myProcess.StartInfo = new ProcessStartInfo("calc.exe");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myProcess.Start();
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show($"Ошибка при запуске калькулятора: {ex.Message}","Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+            }   
+        }
+
+        private void process1_Exited(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Закрытие главного окна процесса
+                myProcess.CloseMainWindow();
+
+                // Освобождаем память
+                myProcess.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при остановке калькулятора: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
